3a90bb0b3ea3e70adc7d30cac6d01407cb36151e
saketh1277 <48929140+saketh1277@users.noreply.github.com>
2020-06-23 12:16:52 -0400

    removed secrets

--- BaseInfra_code/terraform/providers.tf
+++ BaseInfra_code/terraform/providers.tf
@@ -1,10 +1,10 @@
 
 
 provider "azurerm" {
-  client_secret   = "wznXKPY70b64rnjZTn=NhMqOnAuVl@=]"
-  subscription_id = "34d9e027-3f29-417d-be50-fd75c0472242"
-  tenant_id       = "b9fec68c-c92d-461e-9a97-3d03a0f18b82"
-  client_id       = "b2573f9c-e97c-425f-973b-0b873f259f21"
+  client_secret   = ""
+  subscription_id = ""
+  tenant_id       = ""
+  client_id       = ""
   features {}
 
 }
@@ -14,10 +14,10 @@ provider "azurerm" {
 # Azure AD Provider
 provider "azuread" {
 
-  client_secret   = "wznXKPY70b64rnjZTn=NhMqOnAuVl@=]"
-  subscription_id = "34d9e027-3f29-417d-be50-fd75c0472242"
-  tenant_id       = "b9fec68c-c92d-461e-9a97-3d03a0f18b82"
-  client_id       = "b2573f9c-e97c-425f-973b-0b873f259f21"
+  client_secret   = ""
+  subscription_id = ""
+  tenant_id       = ""
+  client_id       = ""
 
 
 }
